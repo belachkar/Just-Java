8192e08ab7e7be7c0eb7b895be085a6038169a62
belachkar ali <34995109+belachkar@users.noreply.github.com>
2019-09-21 00:45:06 +0100

    Add order function button

--- app/src/main/java/io/github/belachkar/justjava/MainActivity.java
+++ app/src/main/java/io/github/belachkar/justjava/MainActivity.java
@@ -1,8 +1,12 @@
 package io.github.belachkar.justjava;
 
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    /*
+     * This method is called when the Order button is clicked
+     */
+    public void submitOrder(View view) {
+        display(1);
+    }
+
+    /*
+     * Display the given quantity on the screen
+     */
+    private void display(int number) {
+        TextView quantityTextView = findViewById(R.id.quantity_nbr);
+        quantityTextView.setText(String.format(Locale.US, "%d", number));
     }
 }
