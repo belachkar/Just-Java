46bc8d0aed03c281171616ed9a7967eb0282d2bd
belachkar ali <34995109+belachkar@users.noreply.github.com>
2019-09-22 04:26:41 +0100

    Add checkbox "Choclate"; Add total price

--- app/src/main/java/io/github/belachkar/justjava/MainActivity.java
+++ app/src/main/java/io/github/belachkar/justjava/MainActivity.java
@@ -1,9 +1,9 @@
 package io.github.belachkar.justjava;
 
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.CheckBox;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -14,14 +14,17 @@ import java.text.NumberFormat;
 import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
-    private String name = "Belachkar Ali";
-    private Boolean hasWhippedCream = false;
     private int qty = 2;
     private int qtyUnits = 1;
-    private int unitPrice = 5;
+    private int coffeePrice = 5;
+    private int whippedCreamPrice = 1;
+    private int chocolatePrice = 2;
 
+    private EditText nameEditText;
     private TextView orderSummaryTextView;
     private TextView qtyTextView;
+    private CheckBox hasWhippedCreamCheckBox;
+    private CheckBox hasChocolateCheckBox;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,8 +32,17 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         // Get the Views pointers
+        nameEditText = findViewById(R.id.name_field);
         orderSummaryTextView = findViewById(R.id.order_summary_text_view);
         qtyTextView = findViewById(R.id.quantity_text_view);
+        hasWhippedCreamCheckBox = findViewById(R.id.whippedCreamCheckBox);
+        hasChocolateCheckBox = findViewById(R.id.chocolateCheckBox);
+
+        // Initialize parameter values
+        orderSummaryTextView.setText("");
+        qtyTextView.setText(String.format(Locale.US, "%d", qty));
+        hasWhippedCreamCheckBox.setChecked(false);
+        hasChocolateCheckBox.setChecked(false);
 
         // Display initial Quantity value
         displayQty();
@@ -40,10 +52,13 @@ public class MainActivity extends AppCompatActivity {
      * This method is called when the Order button is clicked
      */
     public void submitOrder(View view) {
-        int price = calculatePrice();
-        Log.v("MainActivity", "The price is " + price);
+        boolean hasWhippedCream = hasWhippedCreamCheckBox.isChecked();
+        boolean hasChocolate = hasChocolateCheckBox.isChecked();
 
-        String summary = createOrderSummary(price);
+        int price = calculatePrice(hasWhippedCream, hasChocolate);
+        String name = nameEditText.getText().toString();
+
+        String summary = createOrderSummary(price, hasWhippedCream, hasChocolate, name);
         displayMessage(summary);
     }
 
@@ -60,21 +75,32 @@ public class MainActivity extends AppCompatActivity {
      * @return total price
      */
     @org.jetbrains.annotations.Contract(pure = true)
-    private int calculatePrice() {
-        return qty * unitPrice;
+    private int calculatePrice(boolean hasWhippedCream, boolean hasChocolate) {
+        int price = 0;
+        if (qty > 0) {
+            price += coffeePrice;
+            price += hasChocolate ? chocolatePrice : 0;
+            price += hasWhippedCream ? whippedCreamPrice : 0;
+            price *= qty;
+        }
+        return price;
     }
 
     /**
      * Create summary of the order
      *
-     * @param totalPrice of the prder
+     * @param price           of the order.
+     * @param hasWhippedCream is whether or not the user wants Whipped cream.
+     * @param hasChocolate    is whether or noe the user wants Chocolate.
+     * @param name            The name of the user.
      * @return text summary
      */
-    private String createOrderSummary(int totalPrice) {
+    private String createOrderSummary(int price, boolean hasWhippedCream, boolean hasChocolate, String name) {
         String summary = "Name:\t\t\t\t\t" + name;
         summary += "\n" + "Add whipped cream? " + hasWhippedCream;
+        summary += "\n" + "Add chocolate? " + hasChocolate;
         summary += "\n" + "Quantity:\t\t\t" + qty;
-        summary += "\n" + "Total Price:\t\t" + NumberFormat.getCurrencyInstance().format(totalPrice);
+        summary += "\n" + "Total Price:\t\t" + NumberFormat.getCurrencyInstance().format(price);
         summary += "\n\n" + "Thank you!";
         return summary;
     }
@@ -98,11 +124,5 @@ public class MainActivity extends AppCompatActivity {
     private void makeOrderSummaryUnfocused() {
         orderSummaryTextView.setTextColor(0xff999999);
     }
-
-    public void onWhippedCreamClicked(View view) {
-        Log.v("MainActivity", "The whipped cream check box is clicked: " + hasWhippedCream);
-        hasWhippedCream = ((CheckBox) view).isChecked();
-    }
-
 
 }
